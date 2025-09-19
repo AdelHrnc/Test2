582c59156cc3bdbb5f0257bd99eec6042e169db6
AdelHrnc <140653656+AdelHrnc@users.noreply.github.com>
2025-09-18 21:04:03 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'username' with your GitHub username, and 'cst-drc-web' with your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/cst-drc-web/', 
+  base: './', 
 })
